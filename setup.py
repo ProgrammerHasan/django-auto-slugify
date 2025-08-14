ff810d7781ee5b4df0ad610a53792854805256c6
Mehedi Hasan <44167334+ProgrammerHasan@users.noreply.github.com>
2025-08-14 22:53:02 +0600

    django-auto-slugify

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-auto-slugify",
-    version="0.1.0",
+    version="0.1.1",
     author="ProgrammerHasan",
     author_email="programmerhasan@email.com",
     description="Auto-generate unique slugs for Django models",
